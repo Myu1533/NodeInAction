5a360b3977e36ebfa41b55ee36952bcb9f8c92f6
Man Yu <diabolos.man@gmail.com>
2017-12-05 10:31:19 +0800

    register init

---
+++ registInAction/routes/login.js
@@ -0,0 +1,21 @@
+var User = require('../lib/user')
+
+exports.form = function (req, res) {
+  res.render('login', {
+    title: 'Login'
+  })
+}
+
+exports.submit = function(req,res,next){
+  var data = req.body.user
+  User.authenticate(data.name,data.pass, function(err, user){
+    if(err) return next(err)
+    if(user){
+      req.session.uid = user.id
+      res.redirect('/')
+    }else{
+      res.error('Sorry! invalid credentials.')
+      ewa.redirect('back')
+    }
+  })
+}
